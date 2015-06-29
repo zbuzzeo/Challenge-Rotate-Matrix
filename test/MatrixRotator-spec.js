6fe08255bce71e355da7a71e1e3d03dcdf4b11cf
Jon Borgonia <jon@gomagames.com>
2015-06-29 08:19:44 -1000

    converted jasmine tests to mocha chai

--- test/MatrixRotator-spec.js
+++ test/MatrixRotator-spec.js
@@ -1,3 +1,5 @@
+var chai = require('chai');
+chai.should();
 var MatrixRotator = require("../MatrixRotator").MatrixRotator;
 var Matrix = require("../Matrix").Matrix;
 var Direction = require("../Direction").Direction;
@@ -8,7 +10,7 @@ describe("The Matrix Rotator", function () {
   
   it("can rotate Clockwise", function () {
     matrixRotator.rotate(Direction.CW);
-    expect(matrixRotator.matrix).toEqual( [
+    matrixRotator.matrix.should.be.deep.equal( [
                                             [9,0,3,4,8],
                                             [0,6,3,5,0],
                                             [3,8,6,2,1],
@@ -21,7 +23,7 @@ describe("The Matrix Rotator", function () {
   it("can rotate CounterClockwise", function () {
     matrixRotator.rotate(Direction.CCW);
     matrixRotator.rotate(Direction.CCW);
-    expect(matrixRotator.matrix).toEqual([
+    matrixRotator.matrix.should.be.deep.equal([
                                             [3,7,0,3,7],
                                             [9,9,9,8,8],
                                             [1,2,6,8,3],
