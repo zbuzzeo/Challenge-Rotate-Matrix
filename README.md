a3705070fb89ea2fc55f4c924871831421f2d204
Jon Borgonia <jon@gomagames.com>
2015-06-29 08:48:00 -1000

    adds instructions for advanced exercise

--- README.md
+++ README.md
@@ -28,6 +28,31 @@ f e d
 c b a
 ```
 
+## Advanced Implementation
+
+`MatrixRotator.rotate` will accept an optional second argument.
+
+The default (omitting the second argument) is to maintain the original rotating behavior (rotate all layers).
+
+The second argument will determine which 'layer' will be rotated.
+
+A layer is the set of elements belonging to the same distance from the center.
+
+Example, the values are what 'layer' that position belongs to:
+
+```
+3 3 3 3 3 3
+3 2 2 2 2 3
+3 2 1 1 2 3
+3 2 1 1 2 3
+3 2 2 2 2 3
+3 3 3 3 3 3
+```
+
+If the second argument is set, only that layer should be rotated.
+
+If the second argument is a value below `1` or greater than `'radius' + 1` throw a RangeError
+
 ## Install dependencies
 
 ```
--- test/MatrixRotator-spec.js
+++ test/MatrixRotator-spec.js
@@ -6,7 +6,10 @@ var Direction = require("../Direction").Direction;
 
 describe("The Matrix Rotator", function () {
   
-  var matrixRotator = new MatrixRotator(Matrix.matrix1);
+  var matrixRotator;
+  beforeEach(function(){
+    matrixRotator = new MatrixRotator(Matrix.matrix1);
+  });
   
   it("can rotate Clockwise", function () {
     matrixRotator.rotate(Direction.CW);
@@ -22,13 +25,69 @@ describe("The Matrix Rotator", function () {
 
   it("can rotate CounterClockwise", function () {
     matrixRotator.rotate(Direction.CCW);
-    matrixRotator.rotate(Direction.CCW);
     matrixRotator.matrix.should.be.deep.equal([
                                             [3,7,0,3,7],
                                             [9,9,9,8,8],
                                             [1,2,6,8,3],
                                             [0,5,3,6,0],
                                             [8,4,3,0,9]
+                                          ]);
+  });
+
+});
+describe("The Advanced Matrix Rotator", function () {
+  
+  var matrixRotator;
+  beforeEach(function(){
+    matrixRotator = new MatrixRotator(Matrix.matrix2);
+  });
+  
+  it("can rotate all layers Clockwise", function () {
+    matrixRotator.rotate(Direction.CW);
+    matrixRotator.matrix.should.be.deep.equal( [
+                                            [31,25,19,13,7,1],
+                                            [32,26,20,14,8,2],
+                                            [33,27,21,15,9,3],
+                                            [34,28,22,16,10,4],
+                                            [35,29,23,17,11,5],
+                                            [36,30,24,18,12,6]
+                                          ]);
+
+  });
+
+  it("can rotate layer 1 CounterClockwise", function () {
+    matrixRotator.rotate(Direction.CCW, 1);
+    matrixRotator.matrix.should.be.deep.equal([
+                                            [1, 2, 3, 4, 5, 6],
+                                            [7, 8, 9, 10,11,12],
+                                            [13,14,16,22,17,18],
+                                            [19,20,15,21,23,24],
+                                            [25,26,27,28,29,30],
+                                            [31,32,33,34,35,36]
+                                          ]);
+  });
+
+  it("can rotate layer 2 Clockwise", function () {
+    matrixRotator.rotate(Direction.CW, 2);
+    matrixRotator.matrix.should.be.deep.equal([
+                                            [1 ,2 ,3 ,4 ,5 ,6 ],
+                                            [7 ,26,20,14,8 ,12],
+                                            [13,27,15,16,9 ,18],
+                                            [19,28,21,22,10,24],
+                                            [25,29,23,17,11,30],
+                                            [31,32,33,34,35,36]
+                                          ]);
+  });
+
+it("can rotate layer 3 Clockwise", function () {
+    matrixRotator.rotate(Direction.CW, 3);
+    matrixRotator.matrix.should.be.deep.equal([
+                                            [31,25,19,13,7 ,1 ],
+                                            [32,8 ,9 ,10,11,2 ],
+                                            [33,14,15,16,17,3 ],
+                                            [34,20,21,22,23,4 ],
+                                            [35,26,27,28,29,5 ],
+                                            [36,30,24,18,12,6 ]
                                           ]);
   });
 
